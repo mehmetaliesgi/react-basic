603add18bc71fbe5369cfd8ab00eb2b55bdcb706
Mehmet Ali Eşgi <mehmetaliesgi@gmail.com>
2024-09-25 00:37:08 +0300

    CountryInformation component complated. The component fetch country data and send with props to CountryInfo component

--- country-information/src/components/CountryInfo.jsx
+++ country-information/src/components/CountryInfo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function CountryInfo() {
+function CountryInfo({ countryData }) {
   return <div>CountryInfo</div>;
 }
 
--- country-information/src/components/CountryInformation.jsx
+++ country-information/src/components/CountryInformation.jsx
@@ -1,7 +1,39 @@
 import CountryInfo from "./CountryInfo";
 import "../css/countryInformation.css";
+import { useState } from "react";
 
 function CountryInformation() {
+  const [countryName, setCountryName] = useState("");
+  const [countryData, setCountryData] = useState(null);
+  const [error, setError] = useState("");
+
+  const searchCountry = () => {
+    if (!countryName) {
+      setError("You can't empty this area!");
+      setCountryData(null);
+      return;
+    }
+    const URL = `https://restcountries.com/v3.1/name/${countryName.trim()}?fullText=true`;
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "Not Found") {
+          setError("Country Information is not Found");
+          setCountryData(null);
+        } else if (data.length === 0) {
+          setError("Please enter valid country name!");
+          setCountryData(null);
+        } else {
+          setError("");
+          setCountryData(data[0]);
+        }
+      })
+      .catch(() => {
+        setError("An error occurred while fetching data.");
+        setCountryData(null);
+      });
+  };
+
   return (
     <div className="countryDiv">
       <div className="countryElements">
@@ -9,11 +41,18 @@ function CountryInformation() {
           className="countryInput"
           type="text"
           placeholder="Enter a country name"
+          value={countryName}
+          onChange={(e) => {
+            setCountryName(e.target.value);
+          }}
         />
-        <button className="countryButton">Search</button>
+        <button className="countryButton" onClick={searchCountry}>
+          Search
+        </button>
       </div>
       <div className="result">
-        <CountryInfo />
+        {error && <h3>{error}</h3>}
+        {countryData && <CountryInfo countryData={countryData} />}
       </div>
     </div>
   );
