4891c0b7585967c61dc0262ebc74f12c955f53e6
Mehmet Ali Eşgi <mehmetaliesgi@gmail.com>
2024-10-05 14:54:43 +0300

    Components created and tailwindcss added the project

---
+++ redux-todo/src/components/Task.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Task() {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Add new task" />
+        <button>Add</button>
+      </div>
+    </div>
+  );
+}
+
+export default Task;
